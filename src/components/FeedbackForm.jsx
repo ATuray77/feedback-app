dfe00844c6a15be59afc1574f3197dbd7a08aaa1
Aruna Turay <tura_arun@yahoo.com>
2024-02-07 17:28:22 -0500

    adds Feedback Form

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,11 @@
+import Card from "./shared/Card"//because we want to wrap the form in a card
+
+function FeedbackForm() {
+  return (
+    <Card>
+      Form
+    </Card>
+  )
+}
+
+export default FeedbackForm

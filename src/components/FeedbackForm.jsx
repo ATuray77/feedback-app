3c17d9e255324efe73855170b26e5d69d04b844b
Aruna Turay <tura_arun@yahoo.com>
2024-02-07 17:41:56 -0500

    adds event lintener to the input field and set state to monitor changes to the text change

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react"//to handle input states for our form
 import Card from "./shared/Card"//because we want to wrap the form in a card
 
+
 function FeedbackForm() {
+
+const [text, setText] = useState('')
+
+const handleTextChange = (e) => {
+    setText(e.target.value)
+}
+
   return (
     <Card>
-      Form
+      <form> 
+        <h2>How would you rate your service with us
+        </h2>
+        {/* @todo - rating select component */}
+        <div className="input-group">
+            <input 
+            onChange={handleTextChange}
+            type="text"  
+            placeholder="Write a review"
+            value= {text}/>
+            <button type="submit">Send</button>
+        </div>
+      </form>
     </Card>
   )
 }

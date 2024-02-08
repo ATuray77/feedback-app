ce852269419375cc15afab621e213d78ef012ae3
Aruna Turay <tura_arun@yahoo.com>
2024-02-08 17:33:54 -0500

    adds UUId as unique id for the list items

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,7 +5,7 @@ import Button from "./shared/Button"
 
 
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
 
 const [text, setText] = useState('')
 const [rating, setRating] = useState(10)
@@ -27,9 +27,21 @@ const handleTextChange = (e) => { ///real-time validation
     setText(e.target.value)
 }
 
+const handleSubmit = (e) => {
+  e.preventDefault()
+  if (text.trim().length > 10){//if the text length is greather than 10 create a new feed back object
+    const newFeedback = {
+      text,
+      rating
+    }
+    handleAdd(newFeedback)
+    setText('')
+  }
+}
+
   return (
     <Card>
-      <form> 
+      <form onSubmit={handleSubmit}> 
         <h2>How would you rate your service with us
         </h2>
         <RatingSelect select={(rating) => setRating(rating)} />

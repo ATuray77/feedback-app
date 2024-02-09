2d13ff3a098b8d867a206978c268fd37948d1a86
Aruna Turay <tura_arun@yahoo.com>
2024-02-09 11:05:46 -0500

    adds AboutLink component

---
+++ src/components/AboutIconLink.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaQuestion } from 'react-icons/fa'
+
+function AboutIconLink() {
+  return (
+    <div>
+      <FaQuestion size={30} />
+    </div>
+  )
+}
+
+export default AboutIconLink

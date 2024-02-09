f399d69e8c76e4dd7d20788b8be3987e57b3f694
Aruna Turay <tura_arun@yahoo.com>
2024-02-09 11:15:55 -0500

    adds a link on quetion mark that will redirect to the home page and back to the components page

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FaQuestion } from 'react-icons/fa'
 
-function AboutIconLink() {
+function AboutIconLink() { //this will position it absolute down at the corner
   return (
-    <div>
-      <FaQuestion size={30} />
+    <div className='about-link'>
+        <a href='/about'>
+         <FaQuestion size={30} />
+         </a>
     </div>
   )
 }

a019ebf605d5aff8600163952b8738bf9d78e73a
Aruna Turay <tura_arun@yahoo.com>
2024-02-10 09:20:21 -0500

    brings in FAEdit from fontawesome

--- src/context/FeedBackContext.js
+++ src/context/FeedBackContext.js
@@ -1,5 +1,7 @@
 import { v4 as uuidv4} from 'uuid'
-import { createContext,useState } from "react"
+import { createContext, useState } from "react"
+
+
 const feedbackContext = createContext()
 
 //inorder for our components to get access 'state' and 'context'we wrap every component in a provider. 'children' are the components that's being passed in which are going to need access to context
@@ -8,17 +10,17 @@ export const FeedbackProvider = ({children}) => {
     const [feedback, setFeedback] = useState([ //this is where state is stored
         {
             id: 1,
-            text: 'This item is feedback item 1',
+            text: 'I am Roliza, I love this product 1',
             rating: 10,
         },
         {
             id: 2,
-            text: 'This item is feedback item 2',
+            text: 'I am Roliza, I do not really like this product 2',
             rating: 10,
         },
         {
             id: 3,
-            text: 'This item is feedback item 3',
+            text: 'I am Roliza! This item is a junk, so nasty 3',
             rating: 10,
         }
     ])
@@ -36,7 +38,7 @@ export const FeedbackProvider = ({children}) => {
     }
 
     return <feedbackContext.Provider value={{ //pass in the data that needs passing
-        feedback: feedback,
+        feedback,
         deleteFeedback,
         addFeedback,
     }}>
